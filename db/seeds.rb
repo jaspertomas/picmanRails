0656c7a8bf821f26440e22d2d4984effa8175ff6
Jasper Tomas <jaspertomas@gmail.com>
2021-06-09 17:02:10 +0800

    added seed 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 product1=Product.create!(name:"product1")
 product1.gen_image_from_path(path:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSNG7XDGjC4LjvDZt9egxGrq-oIHgSMAEftk02sPnxTKfrI-bDl', filename: 'kidlat_tahimik.jpg', content_type: 'image/jpg')
+
+product2=Product.create!(name:"product2")
+product2.gen_image_from_path(path:'https://2.bp.blogspot.com/-tlR5gS3g-To/WaZN0wd5Y6I/AAAAAAAEC-c/nn2yJVh4SZ4v5Mn3v9aS2QeQ_ap7RsG2ACLcBGAs/s1600/Anita%2BMagsaysay-Ho-photo01.jpg', filename: 'anita_magsaysay.jpg', content_type: 'image/jpg')
