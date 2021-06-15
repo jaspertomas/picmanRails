56cf88da6c6e9b6b2cd6cb70570e2e5c2318a234
Jasper Tomas <jaspertomas@gmail.com>
2021-06-15 16:12:16 +0800

    generous convert works

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -88,10 +88,10 @@ class ProductsController < ApplicationController
     #path = d:/src/picmanRails/storage/6a/3w/6a3wn80hov9h1nx3pct9qrli8ynk
 
     # remove background from source image
-    success=system("cd D:\\src\\image-background-removal & python3 seg.py "+path+" "+filename+".png"+" 1")
+    success=system("cd D:\\src\\image-background-removal & python3 seg.py "+path+" output\\"+filename+".png"+" 1")
     if(success)
       # attach image with background removed
-      @product.gen_image_from_path(path:"D:\\src\\image-background-removal\\"+filename+".png", filename: filename+'.png', content_type: 'image/png')
+      @product.gen_image_from_path(path:"D:\\src\\image-background-removal\\output\\"+filename+".png", filename: filename+'.png', content_type: 'image/png')
     end
 
     respond_to do |format|
